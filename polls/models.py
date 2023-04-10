fa5e708d81b1f53e2cbbecd9fb5358cf009ef9cb
checho rios <chechorios2008@gmail.com>
2023-04-09 23:30:57 -0500

    Creación app polls, para la gestión de encuestas

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,24 @@
+import datetime
 from django.db import models
+from django.utils import timezone 
 
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=250)
+    pub_date = models.DateTimeField("date plublished")
+
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >=timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=250)
+    votes = models.IntegerField(default=0) #Variable contador.
+
+    def __str__(self):
+        return self.choice_text   
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
-
+from . models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello, yo de nuevo dandole a Django.')
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list":latest_question_list
+    })
+
+
+def detail(request, question_id):
+    question = get_list_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question":question
+    })
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados número {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta número {question_id}")
